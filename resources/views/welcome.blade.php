9cbd83f4cef5269cdb5eec040a63aef088be661d
Domenico Lobusto <domenico.lobusto86@gmail.com>
2025-05-24 21:21:45 +0200

    modifica navbar,layout, web

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<x-layout>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-12 text-center">
+                
+                <h1>Homepage</h1>
+            
+            </div>
+            
+        </div>
+    </div>
+    
+</x-layout>

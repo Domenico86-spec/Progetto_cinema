657a798c73d20b54ee5c06833164adffcd73b5f2
Domenico Lobusto <domenico.lobusto86@gmail.com>
2025-06-16 18:18:44 +0200

    risolto errore textarea

--- resources/views/article/create.blade.php
+++ resources/views/article/create.blade.php
@@ -7,38 +7,33 @@
         </div>
         <div class="row justify-content-center mt-5">
             <div class="col-12 col-md-6">
-                <form method="POST" action="{{route('store')}}" enctype="multipart/form-data">
+                <form method="POST" action="{{route('article.store')}}" enctype="multipart/form-data">
                     @csrf
                     <div class="mb-3">
                                 <label for="exampleInputEmail1" class="form-label">Titolo</label>
                                 <input type="text" class="form-control"  name="title">
                             </div>
-                            
+                             <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Film</label>
+                                <select name="film" id="">
+                                    @foreach ($movies as $movie)
+                                    <option value="{{$movie->id}}">{{$movie->name}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
                             <div class="mb-3">
                                 <label for="exampleInputPassword1" class="form-label">Immagine</label>
                                 <input type="file" class="form-control" id="exampleInputPassword1" name="image">
                             </div>
                             <div class="mb-3">
                                 <label for="body" class="form-label">Descrizione </label>
-                                <textarea name="body" class="form-control" id="body" cols="30" rows="10"></textarea">
+                                <textarea name="body" class="form-control" id="body" cols="30" rows="10"></textarea>
                             </div>
-                             <div class="mb-3">
-                                <label for="exampleInputPassword1" class="form-label">Movie</label>
-                                <select name="movie" id="m" class="form-control">
-                                    @foreach ($movies as $movie)
-                                    <option value="{{$movie->id}}">{{$movie->name}}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                            <div class="mt-3 d-flex justify-content-center flex-column align-items-center">
-
+                            
+                            
+                            
                                 <button type="submit" class="btn btn-primary">Inserisci articolo</button>
-                                <a href="{{route('homepage')}}" class="btn btn-primary mt-2">Torna alla Home</a>
-                            </div>
-                            
-                            
-                            
-                            
+                           
                 </form>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,8 @@ use App\Http\Controllers\ArticleController;
 
 Route::get('/', [ArticleController::class, 'index'])->name('homepage');
 Route::get('/article/create', [ArticleController::class, 'create'])->name('article.create');
-Route::post('/article/store', [ArticleController::class, 'store'])->name('store');
+Route::post('/article/store', [ArticleController::class, 'store'])->name('article.store');
+
 
 
 
